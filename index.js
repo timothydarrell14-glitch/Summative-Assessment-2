c081c77a3f54d7dad8ecd531f5169e6fc1ef69b7
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-04-16 12:47:02 +0300

    feat: refactor index.html and index.js; remove unused sections and enhance history display functionality

--- index.js
+++ index.js
@@ -4,43 +4,6 @@ const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 //          Search section ----------------------------
 
 
-//    Search word function (api request) and alert to display  +  search button event + press enter event
-
-let b_search = document.getElementById('search')
-
-let s_word = document.getElementById('search_word')
-
-    b_search.addEventListener('click', async () => {
-        
-        let input = document.getElementById('search_word').value
-
-        addHistory(input)
-
-        console.log(input)
-
-    try {
-        let response = await fetch(`${BASE_URL}${input}`)
-
-        if (!response.ok) {
-            throw new Error("Network Error")
-        }
-
-        let word = await response.json()
-
-        let html = 
-        `
-        `
-
-        //  input = ""
-
-        console.log(word)
-        console.log(typeof word)
-
-    } catch (error) {
-        console.error("Error getting word", error)
-    }
-    })
-
 // Array and function to store and display history
 
 let g_history = document.getElementById('history')
@@ -66,93 +29,167 @@ g_history.addEventListener('click', () => {
     }
 })
 
+//    Search word function (api request) and alert to display  +  search button event + press enter event
 
-//          Word of the Day section ---------------------------
+let b_search = document.getElementById('search')
 
-// speech pronounciation function + word pronounciation function + display
-// random word of the day function + display
-// definition funtion + display
-// retrieve 6 synonyms function +display
+let s_word = document.getElementById('search_word')
 
+b_search.addEventListener('click', async () => {
 
-//          Vocab finds section ---------------------------
+    let input = document.getElementById('search_word').value
 
-// extra section filter function + definition + examples +display
+    addHistory(input)
 
+    console.log(input)
 
+    try {
+        let response = await fetch(`${BASE_URL}${input}`)
 
+        if (!response.ok) {
+            throw new Error("Network Error")
+        }
 
+        let word = await response.json()
 
+        let html =
+            `<div id="output">
+                <div>
+                    <div>
+                        <h3>${input}</h3>
+                    </div>
+                    <div id="wrd">
+                        <button id="btn"><i id="word-pronounce" class="fa-solid fa-volume"
+                                href="pronounce-search"></i>${word.phonetics}</button>
+                        <p id="ph">${word.phonetic}</p>
+                    </div>
+                    <div>
+                        <p id="wrd-def">(${word.meanings})</p>
+                    </div>
+                    <div>
+                        <p id="wrd-ex"></p>
+                    </div>
+                </div>
+            </div>`
 
+        document.getElementById('src-wrd').innerHTML += html
 
+        input = ""
 
+        console.log(word)
+        console.log(typeof word)
 
-
-
-
-
-
-
-
-
-
-
-
-// IDEAS-------------------------------
-
-// function keyPress(event){
-//     if(event.value === "Enter"){
-//         console.log("Key Pressed")
-//     }
-// }
-
-// s_word.addEventListener("keydown", function(event){
-//     if(event.value === "Enter"){
-//         async () => {
-
-//             let input = document.getElementById('search_word').value
-
-//     console.log(input)
-
-//     try {
-//         let response = await fetch(`${BASE_URL}${input}`)
-
-//         if (!response.ok) {
-//             console.log("Network Error")
-//         }
-
-//         let word = await response.json()
-
-//         console.log(word)
-
-//         // function displaySearch(){
-//         //     Swal.fire(
-//         //         `
-//         //         `
-//         //     )
-//         // }
-
-//         input.value = ""
+    } catch (error) {
+        console.error("Error getting word", error)
+    }
+})
 
 
-//     } catch (error){
+    //          Word of the Day section ---------------------------
 
-//     }}}})
+    // speech pronounciation function + word pronounciation function + display
+    // random word of the day function + display
+    // definition funtion + display
+    // retrieve 6 synonyms function +display
 
 
-// Swal.fire(`<div id="pronounce">
-//                         <button aria-label="Pronounciation" id="btn-day"><i class="fa-solid fa-volume"
-//                                 id="day-pronounce" href="pronounce-day"></i>${word.phonetics[2]}</button>
-//                         <h3>${input.value}</h3>
-//                         <p>${word.phonetic}</p>
-//                     </div>
-//                     <div id="defo">
-//                         <span>Definition:${word.meaning.definitions[0]}</span>
-//                     </div>
-//                     <div>
-//                         <p>Example</p>
-//                     </div>`)
+    //          Vocab finds section ---------------------------
 
-//         console.log(displayWord(word))
+    // extra section filter function + definition + examples +display
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // IDEAS-------------------------------
+
+    // function keyPress(event){
+    //     if(event.value === "Enter"){
+    //         console.log("Key Pressed")
+    //     }
+    // }
+
+    // s_word.addEventListener("keydown", function(event){
+    //     if(event.value === "Enter"){
+    //         async () => {
+
+    //             let input = document.getElementById('search_word').value
+
+    //     console.log(input)
+
+    //     try {
+    //         let response = await fetch(`${BASE_URL}${input}`)
+
+    //         if (!response.ok) {
+    //             console.log("Network Error")
+    //         }
+
+    //         let word = await response.json()
+
+    //         console.log(word)
+
+    //         // function displaySearch(){
+    //         //     Swal.fire(
+    //         //         `
+    //         //         `
+    //         //     )
+    //         // }
+
+    //         input.value = ""
+
+
+    //     } catch (error){
+
+    //     }}}})
+
+
+    // Swal.fire(`<div id="pronounce">
+    //                         <button aria-label="Pronounciation" id="btn-day"><i class="fa-solid fa-volume"
+    //                                 id="day-pronounce" href="pronounce-day"></i>${word.phonetics[2]}</button>
+    //                         <h3>${input.value}</h3>
+    //                         <p>${word.phonetic}</p>
+    //                     </div>
+    //                     <div id="defo">
+    //                         <span>Definition:${word.meaning.definitions[0]}</span>
+    //                     </div>
+    //                     <div>
+    //                         <p>Example</p>
+    //                     </div>`)
+
+    //         console.log(displayWord(word))
+
+    // < div id = "h-output" >
+    //     <div>
+    //         <div>
+    //             <h3>${input}</h3>
+    //         </div>
+    //         <div id="h-wrd">
+    //             <button id="h-btn"><i id="word-pronounce" class="fa-solid fa-volume"
+    //                 href="pronounce-search"></i> ${word.phonetics}</button>
+    //             <p id="h-ph">${word.phonetic}</p>
+    //         </div>
+    //         <div>
+    //             <p id="h-wrd-def">(${word.meanings})</p>
+    //         </div>
+    //         <div>
+    //             <p id="h-wrd-ex"></p>
+    //         </div>
+    //     </div>
+    //         </div>
 
 
